22d2c7dc74dbcc449643fe972da425fdaa444fea
dodkirua <pybouttefeux@gmail.com>
2021-09-29 16:48:57 +0200

    js initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let h1 = document.querySelector("h1");
+
+let section_array = document.getElementsByClassName('section_select');
+
+//console.log(section_array.length);
+//console.log(h1);
+if (section_array){
+    for (let i = 0 ; i < section_array.length; i++){
+        //console.log(i);
+        //console.log(section_array[i]);
+        section_array[i].addEventListener('click',section);
+
+        function section() {
+            console.log(this.children[0]);
+        }
+    }
+}
