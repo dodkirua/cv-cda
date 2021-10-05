96197ead0d9b1c9154fd58aae3582b26acde309a
dodkirua <pybouttefeux@gmail.com>
2021-10-05 09:29:35 +0200

    commit section finis

--- js/query.js
+++ js/query.js
@@ -2,24 +2,28 @@
 
 $(document).ready(function() {
     $("h1")
-        .animate({fontSize : "2rem"},5000)
-        .animate({fontSize : "1.5rem"},5000)
-        /*.queue(function () {
+        .animate({fontSize : "2rem"},2000)
+        .animate({fontSize : "1.5rem"},2000)
+        .queue(function () {
             $('h2')
                 .animate({marginLeft: '20rem', width: "50%"}, 1500)
-                .animate({marginLeft: '3rem', width: "80%"}, 1500)
-        })*/
-        .queue(function () {
-            $('h3').hide()
-            setTimeout(show,1000);
-
+                .animate({marginLeft: '1rem', width: "80%"}, 1500);
+            $('h3').animate({fontSize: '1.5rem'},1000);
         })
     }
 );
 
+$(document).ready(function(){
+    $('.section_select').click(function(){
 
+        if ($(this).children('div').css('display') === 'block'){
+            $(this).children('h2').children('span').html('&#8595; ');
+            $(this).children('div').hide();
+        }
+        else {
+            $(this).children('h2').children('span').html('&#8594; ');
+            $(this).children('div').show()
+        }
+    })
 
-
-function show(){
-    $('h3').show();
-}
+});
